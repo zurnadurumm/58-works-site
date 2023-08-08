addc02eb972610ee8aadf644eb51281b4ced4067
zurnadurumm <kaan.muradov2005@gmail.com>
2023-08-08 13:11:36 +0400

    modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes } from "react-router-dom"
-import Home from "./pages/home"
+import Home from "./pages/home.jsx"
 
 import Terms from "./pages/Terms"
 import { BrowserRouter, Route, Link, Outlet } from "react-router-dom"
