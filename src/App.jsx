fc9aa3d61dac58274e9ad6104277c1fe3baec2a2
zurnadurumm <kaan.muradov2005@gmail.com>
2023-08-10 00:01:52 +0400

    modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,20 @@ import Home from "./pages/home.jsx"
 import { I18nextProvider } from "react-i18next"
 import i18n from '../i18n.js'
 import Terms from "./pages/Terms"
-import { HashRouter, Route, Link, Outlet } from "react-router-dom"
+import { BrowserRouter, Route } from "react-router-dom"
 import Events from "./pages/Events"
 import NavBar from "./components/NavBar"
 import Privacy from "./pages/Privacy"
 import NotFound from "./pages/NotFound"
 
 
+
 function App() {
   return (
     <I18nextProvider i18n={i18n}>
       <div className="scroll-smooth">
-        <HashRouter>
+        <BrowserRouter>
+
           <Routes>
             <Route path="/" element={<NavBar />} />
             <Route index element={<Home />} />
@@ -25,7 +27,7 @@ function App() {
 
           </Routes>
 
-        </HashRouter>
+        </BrowserRouter>
 
       </div>
     </I18nextProvider>
